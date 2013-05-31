a50dd4ba7137fc8f3f2a517c275f09444e483d41
Heath Stewart <heaths@outlook.com>
2013-05-31 14:22:25 -0700

    Updated version for impending release
    Unfortunately, seems I made a decision a while back to update the assembly versions to 3.0 so the release version and assembly versions are off.

--- src/Microsoft.Tools.WindowsInstaller.PowerShell.Test/Properties/AssemblyInfo.cs
+++ src/Microsoft.Tools.WindowsInstaller.PowerShell.Test/Properties/AssemblyInfo.cs
@@ -30,14 +30,5 @@ using System.Resources;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7703A5CF-2562-4DB2-88E3-BAE16109F7DB")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
--- src/Microsoft.Tools.WindowsInstaller.PowerShell/Properties/AssemblyInfo.cs
+++ src/Microsoft.Tools.WindowsInstaller.PowerShell/Properties/AssemblyInfo.cs
@@ -27,16 +27,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("B97720B1-69AA-46e7-BB4D-FE1196FD7C92")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 [assembly: System.CLSCompliant(false)]
 
