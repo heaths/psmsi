641bd5da8f04b2029e3acce88934be7cb76ba7a4
Heath Stewart <heaths@outlook.com>
2014-01-31 09:17:52 -0800

    Rev version to 2.3.0

--- src/Microsoft.Tools.WindowsInstaller.PowerShell.Test/Properties/AssemblyInfo.cs
+++ src/Microsoft.Tools.WindowsInstaller.PowerShell.Test/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Resources;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7703A5CF-2562-4DB2-88E3-BAE16109F7DB")]
 
-[assembly: AssemblyVersion("2.2.1.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
